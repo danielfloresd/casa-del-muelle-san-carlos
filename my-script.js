260a548b9740af7450aab4a92b8d9c23232e571e
danielfloresd <114637208+danielfloresd@users.noreply.github.com>
2022-10-13 12:04:29 -0700

    fix header and footer

---
+++ my-script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  
+    // Adding click event on id div-1
+    // if it clicked then anonymous
+    // Same as above
+    console.log("Hello World");
+    $('#my-header').load('header.html');
+    $('#my-footer').load('footer.html');
+});
